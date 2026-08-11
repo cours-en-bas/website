371358e5fb51e8552c092f3efb7b3d2d38cd21db
cours-en-bas <nathan.kulling@sib.swiss>
2026-08-11 10:14:13 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -10,7 +10,7 @@ const QUESTIONS = [
     answers: ["ile d'elbe"]
   },
   {
-    image: "images/q2.jpg",
+    image: "images/q2.png",
     question: "ça se corse, ce bucolique sommet surplombe un merveilleux lac, et une auberge non moins charmante (malgré certains contretemps pouvant se dérouler dans sa salle à manger), comment s'appelle-t-il (le sommet) ?",
     answers: ["la palette"]
   },
@@ -42,4 +42,4 @@ const QUESTIONS = [
 ];
 
 // Image affichée en plein écran une fois toutes les questions réussies
-const FINAL_IMAGE = "images/final.jpg";
+const FINAL_IMAGE = "images/final.png";
